25db5dd319c458c2ab66df03778652599b2c7c21
DeniFarm <deni.farmasi.103040@gmail.com>
2024-08-03 23:38:02 +0530

    Update shrinkFE.js

--- shrinkFE.js
+++ shrinkFE.js
@@ -2,17 +2,21 @@ const puppeteer = require('puppeteer');
 
 async function processUrl(url) {
   const LID = url.replace('https://shrinkforearn.xyz/', '');
+  console.log(`Processing LID: ${LID}`);
+
   const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
 
   try {
     await page.goto(`https://shrinkforearn.xyz/${LID}`);
+    console.log('Navigated to initial URL');
     await page.waitForTimeout(4000);
 
     // Send ajaxCallMaker("step_1");
     await page.evaluate(() => {
       window.ajaxCallMaker('step_1');
     });
+    console.log('Executed ajaxCallMaker("step_1")');
 
     // Get SID function
     const getSID = () => {
@@ -23,11 +27,14 @@ async function processUrl(url) {
     };
 
     let SID = await page.evaluate(getSID);
+    console.log(`Initial SID: ${SID}`);
 
     if (!SID) {
+      console.log('SID not found, reloading page');
       await page.reload();
       await page.waitForTimeout(4000);
       SID = await page.evaluate(getSID);
+      console.log(`Reloaded SID: ${SID}`);
     }
 
     if (!SID) {
@@ -38,13 +45,14 @@ async function processUrl(url) {
     await page.evaluate((LID, SID) => {
       window.location.href = `https://shrinkforearn.in/${LID}/?sid=${SID}`;
     }, LID, SID);
-
+    console.log(`Redirected to https://shrinkforearn.in/${LID}/?sid=${SID}`);
     await page.waitForTimeout(7000);
 
     let BPLink = await page.evaluate(() => {
       const element = document.querySelector("body > div.container > div > div > div.box-main > ol > div:nth-child(13)");
       return element ? element.querySelector('a')?.href : null;
     });
+    console.log(`Initial BPLink: ${BPLink}`);
 
     if (!BPLink) {
       await page.waitForTimeout(2000);
@@ -52,11 +60,13 @@ async function processUrl(url) {
         const element = document.querySelector("body > div.container > div > div > div.box-main > ol > div:nth-child(13)");
         return element ? element.querySelector('a')?.href : element.innerHTML;
       });
+      console.log(`Retry BPLink: ${BPLink}`);
     }
 
     await browser.close();
     return BPLink;
   } catch (error) {
+    console.error('Error in shrinkFE process:', error);
     await browser.close();
     throw error;
   }
