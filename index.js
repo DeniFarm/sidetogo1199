58b490d7548d20a2efa6ae2d267f09ec595851ff
DeniFarm <deni.farmasi.103040@gmail.com>
2024-08-04 20:37:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,26 +6,28 @@ const controlPassword = 'TIQ6CRdnqyQrkooo55Y73tko003jD5';
 app.get('/api/bypass', async (req, res) => {
   const { password, url } = req.query;
 
-  console.log(`Received request with password: ${password} and url: ${url}`);
-
   if (password !== controlPassword) {
-    console.log('Password did not match');
     return res.status(403).json({ s: false, r: 'Password did not match' });
   }
 
-  if (!url || !url.startsWith('https://shrinkforearn.xyz')) {
-    console.log('Invalid URL');
-    return res.status(400).json({ s: false, r: 'Invalid URL' });
+  if (!url) {
+    return res.status(400).json({ s: false, r: 'URL is required' });
   }
 
   try {
-    const shrinkFE = require('./shrinkFE');
-    const result = await shrinkFE.processUrl(url);
-    console.log('Processed URL successfully, result:', result);
+    let result;
+    if (url.startsWith('https://shrinkforearn.xyz')) {
+      const shrinkFE = require('./shrinkFE');
+      result = await shrinkFE.processUrl(url);
+    } else if (url.startsWith('https://link.evolinks.in')) {
+      const evo = require('./evo');
+      result = await evo.processUrl(url);
+    } else  {
+      throw error
+    }
     res.json({ s: true, r: result });
   } catch (error) {
-    console.error('Error occurred while processing the URL:', error);
-    res.status(500).json({ s: false, r: 'An error occurred while processing the URL' });
+    res.status(500).json({ s: false, r: `An error occurred while processing the URL: ${error.message}` });
   }
 });
 
